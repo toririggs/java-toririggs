225e838269b061e6989b382ac5ba3721acc8ba3b
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-10-16 21:27:38 -0600

    done for tonight, will try more tomorrow

--- collections/src/test/java/com/mycompany/collections/AppTest.java
+++ collections/src/test/java/com/mycompany/collections/AppTest.java
@@ -5,10 +5,6 @@
  */
 package com.mycompany.collections;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -17,61 +13,6 @@ import static org.junit.Assert.*;
  * @author lolak
  */
 public class AppTest {
-    
-    public AppTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of main method, of class App.
-     */
-    @Test
-    public void testMain() throws Exception {
-        System.out.println("main");
-        String[] args = null;
-        App.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of run method, of class App.
-     */
-    @Test
-    public void testRun() {
-        System.out.println("run");
-        App instance = new App();
-        instance.run();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of initList method, of class App.
-     */
-    @Test
-    public void testInitList() {
-        System.out.println("initList");
-        App instance = new App();
-        instance.initList();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of showMenu method, of class App.
@@ -81,18 +22,6 @@ public class AppTest {
         System.out.println("showMenu");
         App instance = new App();
         instance.showMenu();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of showList method, of class App.
-     */
-    @Test
-    public void testShowList() {
-        System.out.println("showList");
-        App instance = new App();
-        instance.showList();
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -120,17 +49,11 @@ public class AppTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of pause method, of class App.
-     */
-    @Test
-    public void testPause() {
-        System.out.println("pause");
-        App instance = new App();
-        instance.pause();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
+    @Test
+    public void test1() {
+        App instance = new App();
+        instance.showMenu();
+        
+    }
 }
