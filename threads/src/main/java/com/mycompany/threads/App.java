cb2606399ee7619b17fa74e7b65d62d8ca1b3867
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-10-26 17:06:44 -0600

    created base for program based off of javapoint.com/synchronization-in-java

--- threads/src/main/java/com/mycompany/threads/App.java
+++ threads/src/main/java/com/mycompany/threads/App.java
@@ -4,18 +4,66 @@
  * and open the template in the editor.
  */
 package com.mycompany.threads;
-
+import java.io.PrintStream;
+import java.util.Scanner;
 /**
  *
  * @author lolak
  */
-public class App {
 
+
+class AreaOfTrapezoid{
+    synchronized int findArea(int b1, int b2, int h){
+        int ret = ((b1 + b2)/2) * h;
+        return ret;
+    }
+    synchronized void printArea(int area){
+        System.out.println("The area is " + area);
+    }
+}
+
+class th extends Thread{
+    int base1, base2, height;
+    AreaOfTrapezoid a;
+    int getB1(){
+        return base1;
+    }
+    void setB1(int val){
+        base1 = val;
+    }
+    int getB2(){
+        return base2;
+    }
+    void setB2(int val){
+        base2 = val;
+    }
+    int getH(){
+        return height;
+    }
+    void setH(int val){
+        height = val;
+    }
+    th(AreaOfTrapezoid a){
+        this.a = a;
+    }
+    public void run(){
+        // add in setting all variables 
+        a.printArea(a.findArea(getB1(), getB2(), getH()));
+    }
+}
+
+public class App {
+    Scanner in = new Scanner(System.in);
+    PrintStream out = System.out;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AreaOfTrapezoid obj = new AreaOfTrapezoid();
+        th t1 = new th(obj);
+        th t2 = new th(obj);
+        t1.start();
+        t2.start();
     }
     
 }
