67519d929d46e878ba0f4939fb9f597f90dc82f5
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-10-27 14:31:40 -0600

    App.java finished

--- threads/src/main/java/com/mycompany/threads/App.java
+++ threads/src/main/java/com/mycompany/threads/App.java
@@ -18,7 +18,7 @@ class AreaOfTrapezoid{
         return ret;
     }
     synchronized void printArea(int area){
-        System.out.println("The area is " + area);
+        System.out.println("The area of the trapezoid is " + area);
     }
 }
 
@@ -47,7 +47,6 @@ class th extends Thread{
         this.a = a;
     }
     public void run(){
-        // add in setting all variables 
         a.printArea(a.findArea(getB1(), getB2(), getH()));
     }
 }
@@ -59,11 +58,87 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AreaOfTrapezoid obj = new AreaOfTrapezoid();
-        th t1 = new th(obj);
-        th t2 = new th(obj);
-        t1.start();
-        t2.start();
+        App app = new App();
+        app.run();
     }
     
+    void run() {
+        AreaOfTrapezoid obj = new AreaOfTrapezoid();
+        int MAX = 5;
+        th t1, t2, t3, t4, t5;
+        out.println("Enter how many threads to run from 1 to 5");
+        int num = in.nextInt();
+        in.nextLine();
+        if (MAX > num && num > 0) {
+            for (int i = 1; i<=num; i++){
+                switch(i) {
+                    case 1:
+                        t1 = new th(obj);
+                        out.println("Enter an integer for base 1 then hit enter: ");
+                        t1.setB1(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for base 2 then hit enter: ");
+                        t1.setB2(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for height then hit enter: ");
+                        t1.setH(in.nextInt());
+                        in.nextLine();
+                        t1.start();
+                        break;
+                    case 2:
+                        t2 = new th(obj);
+                        out.println("Enter an integer for base 1 then hit enter: ");
+                        t2.setB1(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for base 2 then hit enter: ");
+                        t2.setB2(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for height then hit enter: ");
+                        t2.setH(in.nextInt());
+                        in.nextLine();
+                        t2.start();
+                        break;
+                    case 3:
+                        t3 = new th(obj);
+                        out.println("Enter an integer for base 1 then hit enter: ");
+                        t3.setB1(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for base 2 then hit enter: ");
+                        t3.setB2(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for height then hit enter: ");
+                        t3.setH(in.nextInt());
+                        in.nextLine();
+                        t3.start();
+                        break;
+                    case 4:
+                        t4 = new th(obj);
+                        out.println("Enter an integer for base 1 then hit enter: ");
+                        t4.setB1(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for base 2 then hit enter: ");
+                        t4.setB2(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for height then hit enter: ");
+                        t4.setH(in.nextInt());
+                        in.nextLine();
+                        t4.start();
+                        break;
+                    case 5:
+                        t5 = new th(obj);
+                        out.println("Enter an integer for base 1 then hit enter: ");
+                        t5.setB1(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for base 2 then hit enter: ");
+                        t5.setB2(in.nextInt());
+                        in.nextLine();
+                        out.println("Enter an integer for height then hit enter: ");
+                        t5.setH(in.nextInt());
+                        in.nextLine();
+                        t5.start();
+                        break;
+                }
+            }
+        } else {out.println("Number not between 1 and 5, ending program.");}
+    }
 }
