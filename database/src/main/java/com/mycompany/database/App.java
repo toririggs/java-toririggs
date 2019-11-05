459652a8d44b70ba9cf618c4d631db78e7e5e9aa
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-11-05 16:13:11 -0700

    connecting to mysql using https://www.javatpoint.com/example-to-connect-to-the-mysql-database example

--- database/src/main/java/com/mycompany/database/App.java
+++ database/src/main/java/com/mycompany/database/App.java
@@ -9,13 +9,35 @@ package com.mycompany.database;
  *
  * @author lolak
  */
-public class App {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.sql.*;  
+
+public class App{  
+    public static void main(String args[]){  
+        App app = new App();
+        app.run();
     }
     
-}
+    void run(){
+        System.out.println("got here");
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","1111");  
+            //here db is database name, root is username and 1111 is password  
+            Statement stmt=con.createStatement();  
+            ResultSet rs=stmt.executeQuery("select * from test");  
+            while(rs.next())  
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+            con.close();  
+        }catch(Exception e){ System.out.println(e);}  
+    }
+}  
+
+//public class App {
+
+//    public static void main(String[] args) {
+//        MysqlCon connect = new MysqlCon();
+        
+//    }
+
+//}
