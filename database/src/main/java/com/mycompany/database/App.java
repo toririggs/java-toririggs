48e67b0681ff5d3fd350c67777c559a6e3c353f6
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-11-06 16:07:52 -0700

    expanding classes

--- database/src/main/java/com/mycompany/database/App.java
+++ database/src/main/java/com/mycompany/database/App.java
@@ -12,32 +12,39 @@ package com.mycompany.database;
 
 import java.sql.*;  
 
-public class App{  
-    public static void main(String args[]){  
+
+
+public class App {
+    Connection con;
+    
+    public static void main(String[] args) {
         App app = new App();
         app.run();
+        
     }
     
     void run(){
-        System.out.println("got here");
-        try{  
-            Class.forName("com.mysql.jdbc.Driver");  
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","1111");  
-            //here db is database name, root is username and 1111 is password  
+        try{   
+            openConnect();
             Statement stmt=con.createStatement();  
             ResultSet rs=stmt.executeQuery("select * from test");  
             while(rs.next())  
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-            con.close();  
+            closeConnect();
         }catch(Exception e){ System.out.println(e);}  
     }
-}  
-
-//public class App {
-
-//    public static void main(String[] args) {
-//        MysqlCon connect = new MysqlCon();
+    
+    void openConnect() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","1111"); 
+        }catch(Exception e){System.out.print(e);}
         
-//    }
-
-//}
+    }
+    
+    void closeConnect() {
+        try{
+            con.close();
+        }catch(Exception e){System.out.print(e);}
+    }
+}
