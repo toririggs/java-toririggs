225f082c262d483a2e85240891fa982936eae9ac
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-11-07 12:02:28 -0700

    functions finished, tests initialized

--- database/src/main/java/com/mycompany/database/App.java
+++ database/src/main/java/com/mycompany/database/App.java
@@ -10,7 +10,7 @@ package com.mycompany.database;
  * @author lolak
  */
 
-import java.sql.*;  
+import java.sql.*;
 
 public class App {
     Connection con;
@@ -21,13 +21,14 @@ public class App {
     }
     
     void run(){
-        createTable();
-        insert();
-        select();
-        update();
-        select();
-        delete();
-        dropTable();
+        String table = "thank_you";
+        createTable(table);
+        insert(table, "'Albert'", "8", "'Thank you for donating the socks. I like that they have dinosaurs on them'");
+        select(table);
+        update(table, "age", "9", "name", "'Albert'");
+        select(table);
+        delete(table, "id", "2");
+        dropTable(table);
     }
     
     void openConnect() {
@@ -38,11 +39,11 @@ public class App {
         
     }
     
-    void createTable(){
+    void createTable(String tab){
         try{
             openConnect();
             Statement stmt = con.createStatement();
-            stmt.execute("create table if not exists thank_you (\n"
+            stmt.execute("create table if not exists " + tab + " (\n"
                        + "id int NOT NULL AUTO_INCREMENT,"
                        + "name text,"
                        + "age int,"
@@ -52,49 +53,57 @@ public class App {
         }catch(Exception e){System.out.println(e);}
     }
     
-    void select(){
+    void select(String tab){
         try{   
             openConnect();
             Statement stmt=con.createStatement();  
-            ResultSet rs=stmt.executeQuery("select * from thank_you");  
+            ResultSet rs=stmt.executeQuery("select * from " + tab + ";");  
             while(rs.next())  
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4));  
             closeConnect();
         }catch(Exception e){ System.out.println(e);}  
     }
     
-    void insert(){
+    void insert(String tab, String n, String a, String m){
         try{
             openConnect();
             Statement stmt = con.createStatement();
-            stmt.execute("insert into thank_you(name,age,message) values('Albert',8,'Thank you for donating the socks. I like that they have dinosaurs on them.');");
+            stmt.execute("insert into " + tab + "(name,age,message) values(" + n + "," + a + "," + m + ");");
             closeConnect();
         }catch(Exception e){System.out.println(e);}
     }
     
-    void update(){
+    void update(String tab, String colName, String val, String destColName, String destVal){
         try{
             openConnect();
-            Statement stmt = con.createStatement();
-            stmt.execute("update thank_you set age=9 where name='Albert';");
+            if (stringsOk(colName, val) && stringsOk(destColName, destVal)){
+                Statement stmt = con.createStatement();
+                stmt.execute("update " + tab + " set " + colName + "=" + val + " where " + destColName + "=" + destVal + ";");
+            }else{
+                System.out.println("Incorrect syntax. Column names are age, name, or message, and the values for name and message must begin and end with this symbol: ' ");
+            }
             closeConnect();
         }catch(Exception e){System.out.println(e);}
     }
     
-    void delete(){
+    void delete(String tab, String destColName, String destVal){
         try{
             openConnect();
-            Statement stmt = con.createStatement();
-            stmt.execute("delete from thank_you where id=2");
+            if (stringsOk(destColName, destVal)){
+                Statement stmt = con.createStatement();
+                stmt.execute("delete from " + tab + " where " + destColName + "=" + destVal + ";");
+            }else{
+                System.out.println("Incorrect syntax. Column names are id, age, name, or message, and the values for name and message must begin and end with this symbol: ' ");
+            }
             closeConnect();
         }catch(Exception e){System.out.println(e);}
     }
     
-    void dropTable(){
+    void dropTable(String tab){
         try{
             openConnect();
             Statement stmt = con.createStatement();
-            stmt.execute("drop table thank_you;");
+            stmt.execute("drop table " + tab + ";");
             closeConnect();
         }catch(Exception e){System.out.println(e);}
     }
@@ -103,5 +112,16 @@ public class App {
         try{
             con.close();
         }catch(Exception e){System.out.print(e);}
+    }
+    
+    boolean stringsOk(String col, String val){
+        if (col == "name" || col == "message"){
+            boolean ret = (val.startsWith("'") && val.endsWith("'"));
+            return ret;
+        }
+        else if (col == "age" || col == "id"){
+            boolean ret = (!(val.startsWith("'")) && !(val.endsWith("'")));
+            return ret;
+        }else {return false;}
     }
 }
---
+++ database/src/test/java/com/mycompany/database/AppTest.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.database;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lolak
+ */
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class App.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of run method, of class App.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        App instance = new App();
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of openConnect method, of class App.
+     */
+    @Test
+    public void testOpenConnect() {
+        System.out.println("openConnect");
+        App instance = new App();
+        instance.openConnect();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of createTable method, of class App.
+     */
+    @Test
+    public void testCreateTable() {
+        System.out.println("createTable");
+        String tab = "";
+        App instance = new App();
+        instance.createTable(tab);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of select method, of class App.
+     */
+    @Test
+    public void testSelect() {
+        System.out.println("select");
+        String tab = "";
+        App instance = new App();
+        instance.select(tab);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of insert method, of class App.
+     */
+    @Test
+    public void testInsert() {
+        System.out.println("insert");
+        String tab = "";
+        String n = "";
+        String a = "";
+        String m = "";
+        App instance = new App();
+        instance.insert(tab, n, a, m);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of update method, of class App.
+     */
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        String tab = "";
+        String colName = "";
+        String val = "";
+        String destColName = "";
+        String destVal = "";
+        App instance = new App();
+        instance.update(tab, colName, val, destColName, destVal);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of delete method, of class App.
+     */
+    @Test
+    public void testDelete() {
+        System.out.println("delete");
+        String tab = "";
+        String destColName = "";
+        String destVal = "";
+        App instance = new App();
+        instance.delete(tab, destColName, destVal);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of dropTable method, of class App.
+     */
+    @Test
+    public void testDropTable() {
+        System.out.println("dropTable");
+        String tab = "";
+        App instance = new App();
+        instance.dropTable(tab);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of closeConnect method, of class App.
+     */
+    @Test
+    public void testCloseConnect() {
+        System.out.println("closeConnect");
+        App instance = new App();
+        instance.closeConnect();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of stringsOk method, of class App.
+     */
+    @Test
+    public void testStringsOk() {
+        System.out.println("stringsOk");
+        String col = "";
+        String val = "";
+        App instance = new App();
+        boolean expResult = false;
+        boolean result = instance.stringsOk(col, val);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
