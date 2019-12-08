f6d5c4649c1b9a061244a17980e214d01430fd59
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-12-07 20:03:03 -0700

    struggling through database connection

--- MyApplication/app/src/main/java/com/example/myapplication/DBHelper.java
+++ MyApplication/app/src/main/java/com/example/myapplication/DBHelper.java
@@ -1,9 +1,46 @@
 package com.example.myapplication;
 
 import java.sql.*;
+import android.database.Cursor;
+import android.util.Log;
 
 public class DBHelper {
     Connection con;
+    private static final String TAG = "MyMessage";
 
+    void connectUsers() {
+        try {
+            Class.forName("com.mysql.jbdc.Driver");
+            con = DriverManager.getConnection("jbdc:mysql://localhost:3306/users", "root", "1111");
+        }catch(Exception e){Log.i(TAG, e.toString());}
+    }
 
+    void connectStatus() {
+        try {
+            Class.forName("com.mysql.jbdc.Driver");
+            con = DriverManager.getConnection("jbdc:mysql://localhost:3306/status", "root", "1111");
+        }catch(Exception e){System.out.print(e);}
+    }
+
+    void disconnect() {
+        try{
+            con.close();
+        }catch(Exception e){System.out.print(e);}
+    }
+
+    void addUser(String name, String email) {
+        try{
+            Log.i(TAG,"got to add user");
+            connectUsers();
+            Log.i(TAG,"successfully connected");
+            PreparedStatement stmt = con.prepareStatement("insert into users (user_name,email) values (?,?);");
+            Log.i(TAG,"should have created statement");
+            stmt.setString(1, name);
+            stmt.setString(2, email);
+            stmt.executeUpdate();
+            Log.i(TAG,"should have inserted");
+            disconnect();
+            Log.i(TAG,"successfully disconnected");
+        }catch(Exception e){System.out.print(e);}
+    }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,8 +15,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyMessage";
+    DBHelper db = new DBHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +46,49 @@ public class MainActivity extends AppCompatActivity {
                         // where we will be able to change who the item is checked out to
                     }
                 });
-                alertDialog.setNeutralButton("Email", new DialogInterface.OnClickListener() {
+                alertDialog.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {dialog.cancel();}
+                });
+                alertDialog.setNegativeButton("Email", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // where we will be able to email the person the item is checked out to
                     }
                 });
+                alertDialog.show();
+            }
+        });
+
+        Button b4 = findViewById(R.id.button4);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogStyle);
+                final EditText input = new EditText(MainActivity.this);
+                input.setHint("Name");
+                final EditText input2 = new EditText(MainActivity.this);
+                input2.setHint("Email");
+                LinearLayout l = new LinearLayout(MainActivity.this);
+                l.setOrientation(LinearLayout.VERTICAL);
+                alertDialog.setCancelable(true);
+                alertDialog.setTitle("Add a new User");
+                l.addView(input);
+                l.addView(input2);
+                alertDialog.setView(l);
+                alertDialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String value = input.getText().toString().trim();
+                        String value2 = input2.getText().toString().trim();
+                        db.addUser(value, value2);
+                        Toast.makeText(getApplicationContext(), "Added new user " + value + " with email " + value2, Toast.LENGTH_LONG).show();
+                    }
+                });
                 alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // where we can go back
+                        dialog.cancel();
                     }
                 });
                 alertDialog.show();
