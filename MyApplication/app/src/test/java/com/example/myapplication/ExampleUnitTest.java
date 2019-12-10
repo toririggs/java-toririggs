576a81ceaa9f5922b6e63e8ea37b70d06908cf07
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-12-09 22:44:51 -0700

    testing

--- MyApplication/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ MyApplication/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -9,9 +9,33 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+// Testing for DBHelper
 public class ExampleUnitTest {
+    MainActivity ma = new MainActivity();
+    DBHelper db = new DBHelper(ma);
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testAdd() {
+        db.addUser("Bob", "email@example.com");
+    }
+
+    @Test
+    public void testUpdate() {
+        db.updateStatus(1, "READY", 1001);
+    }
+
+    @Test
+    public void testGetUser() {
+        assertNotNull(db.getUser(1));
+    }
+
+    @Test
+    public void testGetItem() {
+        assertNotNull(db.getItem(1));
+    }
+
+    @Test
+    public void testGetUsers() {
+        assertNotNull(db.getUsers());
     }
 }
